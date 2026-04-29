f1c4da1ee48fab614266f31c81858ca2468cc0c2
Yoonho Cha <0119hoya@naver.com>
2026-04-30 00:13:22 +0900

    refact: 외부 의존성 분리를 위한 port 추가

---
+++ src/main/java/com/yeoljeong/tripmate/infrastructure/redis/RefreshTokenPortImpl.java
@@ -0,0 +1,35 @@
+package com.yeoljeong.tripmate.infrastructure.redis;
+
+import com.yeoljeong.tripmate.application.port.RefreshTokenPort;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RefreshTokenPortImpl implements RefreshTokenPort {
+    private static final String REFRESH_TOKEN_PREFIX = "RT:";
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public void saveRefreshToken(String userId, String refreshToken, long timeout, TimeUnit unit) {
+        redisTemplate.opsForValue()
+            .set(REFRESH_TOKEN_PREFIX + userId, refreshToken, timeout,
+                unit);
+    }
+
+    @Override
+    public void deleteRefreshToken(String userId) {
+        redisTemplate.delete(REFRESH_TOKEN_PREFIX + userId);
+
+    }
+
+    @Override
+    public String getRefreshToken(String userId) {
+        return redisTemplate.opsForValue().get(REFRESH_TOKEN_PREFIX + userId);
+    }
+}
