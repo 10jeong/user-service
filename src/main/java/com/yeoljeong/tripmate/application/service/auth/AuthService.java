279fe278ad90f782d4ef2a656243a587d43e0254
Yoonho Cha <0119hoya@naver.com>
2026-04-30 09:26:22 +0900

    refact: 불필요한 import 제거

--- src/main/java/com/yeoljeong/tripmate/application/service/auth/AuthService.java
+++ src/main/java/com/yeoljeong/tripmate/application/service/auth/AuthService.java
@@ -3,8 +3,6 @@ package com.yeoljeong.tripmate.application.service.auth;
 import com.yeoljeong.tripmate.application.dto.command.RefreshTokenCommand;
 import com.yeoljeong.tripmate.application.dto.result.LoginCommand;
 import com.yeoljeong.tripmate.application.dto.result.LoginResult;
-import com.yeoljeong.tripmate.domain.model.User;
-import java.util.Optional;
 
 public interface AuthService {
 
