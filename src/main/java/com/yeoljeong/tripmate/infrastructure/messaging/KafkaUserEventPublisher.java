79f6cb20eec44707f335031b16082cbc781b3bd3
Yoonho Cha <0119hoya@naver.com>
2026-05-01 13:40:11 +0900

    refact: kafk 요구사항 반영 및 설정 파일 추가

--- src/main/java/com/yeoljeong/tripmate/infrastructure/messaging/KafkaUserEventPublisher.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructure/messaging/KafkaUserEventPublisher.java
@@ -2,8 +2,9 @@ package com.yeoljeong.tripmate.infrastructure.messaging;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yeoljeong.tripmate.application.event.UserCreatedEvent;
 import com.yeoljeong.tripmate.application.port.UserEventPublisher;
+import com.yeoljeong.tripmate.event.UserCreatedEvent;
+import com.yeoljeong.tripmate.event.enums.UserTopic;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,16 +15,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaUserEventPublisher implements UserEventPublisher {
 
-    private static final String USER_CREATED_TOPIC = "user.created";
-
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
     private final ObjectMapper objectMapper;
 
     @Override
     public void publish(UserCreatedEvent event) {
         try {
             String message = objectMapper.writeValueAsString(event);
-            kafkaTemplate.send(USER_CREATED_TOPIC, message);
+            kafkaTemplate.send(UserTopic.USER_CREATED_TOPIC, event);
             log.info("UserCreatedEvent published: {}", message);
         } catch (JsonProcessingException e) {
             log.error("UserCreatedEvent 직렬화 실패: {}", e.getMessage());
