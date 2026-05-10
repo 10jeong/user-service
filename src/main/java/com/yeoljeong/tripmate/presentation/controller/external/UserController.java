c983f778fc05d2636af8c59794ffdc67fc38122b
Yoonho Cha <0119hoya@naver.com>
2026-05-10 17:59:15 +0900

    feat: 회원탈퇴 컨트롤러 코드 작성

--- src/main/java/com/yeoljeong/tripmate/presentation/controller/external/UserController.java
+++ src/main/java/com/yeoljeong/tripmate/presentation/controller/external/UserController.java
@@ -3,6 +3,7 @@ package com.yeoljeong.tripmate.presentation.controller.external;
 import com.yeoljeong.tripmate.application.dto.result.UserCreateResult;
 import com.yeoljeong.tripmate.application.dto.result.UserDetailsResult;
 import com.yeoljeong.tripmate.application.service.command.UserCommandService;
+import com.yeoljeong.tripmate.application.service.command.WithdrawalService;
 import com.yeoljeong.tripmate.application.service.query.UserQueryService;
 import com.yeoljeong.tripmate.auth.annotation.LoginUser;
 import com.yeoljeong.tripmate.auth.context.UserContext;
@@ -13,6 +14,7 @@ import com.yeoljeong.tripmate.response.ApiResponse;
 import com.yeoljeong.tripmate.response.constants.CommonSuccessCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,9 +28,10 @@ public class UserController {
 
     private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
+    private final WithdrawalService withdrawalService;
 
     @PostMapping("/signup")
-    public ApiResponse<UserInfoResponse> createMember(
+    public ApiResponse<UserInfoResponse> createUser(
         @Valid @RequestBody UserCreateRequest request) {
         UserCreateResult result = userCommandService.registerUser(request.toCommand());
         return ApiResponse.success(CommonSuccessCode.CREATE, UserInfoResponse.from(result));
@@ -38,5 +41,11 @@ public class UserController {
     public ApiResponse<UserDetailsResponse> getUserDetails(@LoginUser UserContext userContext) {
         UserDetailsResult result = userQueryService.getUserDetails(userContext.userId());
         return ApiResponse.success(CommonSuccessCode.OK, UserDetailsResponse.from(result));
+    }
+
+    @DeleteMapping("/me")
+    public ApiResponse<Void> withdrawUser(@LoginUser UserContext userContext) {
+        withdrawalService.withdraw(userContext.userId());
+        return ApiResponse.success(CommonSuccessCode.OK, null);
     }
 }
