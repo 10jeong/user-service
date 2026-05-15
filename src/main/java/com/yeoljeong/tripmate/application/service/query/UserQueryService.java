ab39923e6ea945d2fa4bcdaa72a16684bb08fa3e
Yoonho Cha <0119hoya@naver.com>
2026-05-15 10:22:10 +0900

    fix: 내부통신시 역직렬화 에러 해소

--- src/main/java/com/yeoljeong/tripmate/application/service/query/UserQueryService.java
+++ src/main/java/com/yeoljeong/tripmate/application/service/query/UserQueryService.java
@@ -4,7 +4,6 @@ import com.yeoljeong.tripmate.application.dto.command.UserSummaryCommand;
 import com.yeoljeong.tripmate.application.dto.result.UserDetailsResult;
 import com.yeoljeong.tripmate.application.dto.result.UserSummaryResult;
 import com.yeoljeong.tripmate.domain.model.User;
-import com.yeoljeong.tripmate.presentation.dto.response.UserSummaryResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
--- src/main/java/com/yeoljeong/tripmate/presentation/controller/internal/UserInternalController.java
+++ src/main/java/com/yeoljeong/tripmate/presentation/controller/internal/UserInternalController.java
@@ -5,8 +5,6 @@ import com.yeoljeong.tripmate.application.dto.result.UserSummaryResult;
 import com.yeoljeong.tripmate.application.service.query.UserQueryService;
 import com.yeoljeong.tripmate.presentation.dto.request.UserSummaryRequest;
 import com.yeoljeong.tripmate.presentation.dto.response.UserSummaryResponse;
-import com.yeoljeong.tripmate.response.ApiResponse;
-import com.yeoljeong.tripmate.response.constants.CommonSuccessCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,9 +20,9 @@ public class UserInternalController {
     private final UserQueryService userQueryService;
 
     @PostMapping
-    public ApiResponse<List<UserSummaryResponse>> getUsersByIds(@RequestBody UserSummaryRequest request) {
+    public List<UserSummaryResponse> getUsersByIds(@RequestBody UserSummaryRequest request) {
         List<UserSummaryResult> results = userQueryService.getUsersByIds(request.toCommand());
-        return ApiResponse.success(CommonSuccessCode.OK, UserSummaryResponse.from(results));
+        return UserSummaryResponse.from(results);
     }
 
 }
